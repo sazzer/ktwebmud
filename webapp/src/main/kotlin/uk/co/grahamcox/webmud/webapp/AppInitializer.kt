aa92041786e03e5735c950c0ff7743c67855ed9c
Graham Cox <graham@grahamcox.co.uk>
2016-05-17 08:03:37 +0100

    Webapp with Spring-based App Initializer to get things started

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/AppInitializer.kt
@@ -0,0 +1,33 @@
+package uk.co.grahamcox.worldbuilder.webapp
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.WebApplicationInitializer
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
+import javax.servlet.ServletContext
+
+/**
+ * Application Initializer for the webapp
+ */
+open class AppInitializer : WebApplicationInitializer {
+    /** The logger to use */
+    private val LOG = LoggerFactory.getLogger(AppInitializer::class.java);
+
+    /**
+     * Handle all of the Startup functionality of the webapp
+     * @param servletContext The Servlet Context to configure
+     */
+    override fun onStartup(servletContext : ServletContext) {
+        LOG.info("Configuring the application")
+
+        val coreContext = AnnotationConfigWebApplicationContext()
+        coreContext.refresh()
+
+        val webappContext = AnnotationConfigWebApplicationContext()
+        webappContext.parent = coreContext
+
+        val servlet = servletContext.addServlet("dispatcher", DispatcherServlet(webappContext))
+        servlet.addMapping("/")
+        servlet.setLoadOnStartup(1)
+    }
+}
