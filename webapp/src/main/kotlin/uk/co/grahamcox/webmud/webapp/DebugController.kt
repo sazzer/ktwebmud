4d8fb6e5dd59cdabe4195bf0eb784dc1ea8cd9bd
Graham Cox <graham@grahamcox.co.uk>
2016-05-18 11:00:09 +0100

    Swapped out the general WebSocket support for using STOMP

--- webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/DebugController.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/DebugController.kt
@@ -1,16 +1,32 @@
 package uk.co.grahamcox.webmud.webapp
 
+import org.slf4j.LoggerFactory
+import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Clock
+import java.time.Instant
 
+/**
+ * Controller to get some debug information out of the server
+ */
 @RestController
 @RequestMapping("/api/debug")
+@MessageMapping("/api/debug")
 class DebugController(private val clock: Clock) {
+    companion object {
+        /** The logger to use */
+        private val LOG = LoggerFactory.getLogger(DebugController::class.java)
+    }
+
     /**
      * Get the current server time
      * @return the current server time
      */
     @RequestMapping("/now")
-    fun now() = clock.instant()
+    @MessageMapping("/now")
+    fun now(): Instant {
+        LOG.debug("Client requested current server time")
+        return clock.instant()
+    }
 }
