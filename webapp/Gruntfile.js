bb31b0a088f737b53d382b33d9e35f1cfa62e3cd
Graham Cox <graham@grahamcox.co.uk>
2016-05-19 15:59:57 +0100

    Added ability to run Grunt to the build

---
+++ webapp/Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+
+    // Keep the plugins in alphabetical order
+    grunt.initConfig({
+    });
+
+    grunt.registerTask('build', []);
+};
