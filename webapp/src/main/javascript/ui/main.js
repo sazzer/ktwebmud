d6492b72c526e3e170b5f24a400fedc9ab405d3a
Graham Cox <graham@grahamcox.co.uk>
2016-05-21 19:47:48 +0100

    Implemented the new functional style of react components

--- webapp/src/main/javascript/ui/main.js
+++ webapp/src/main/javascript/ui/main.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import i18next from 'i18next';
 
-export const MainScreen = React.createClass({
-    render: function() {
-        return (
-            <div className="main">
-                <div className="header">
-                    {i18next.t('header')}
-                </div>
-                <div className="body"></div>
-                <div className="footer"></div>
-            </div>
-        );
-    }
-});
+export const MainScreen = () => (
+    <div className="main">
+        <div className="header">
+            {i18next.t('header')}
+        </div>
+        <div className="body"></div>
+        <div className="footer"></div>
+    </div>
+);
+
+MainScreen.propTypes = {};
