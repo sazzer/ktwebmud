8b6f8bb8d089629c67acbf282dc16eefe50170e0
Graham Cox <graham@grahamcox.co.uk>
2016-05-28 13:18:10 +0100

    Moved the i18n setup into a separate file

---
+++ webapp/src/main/javascript/components/i18n/setup.js
@@ -0,0 +1,42 @@
+import i18next from 'i18next';
+import XHR from 'i18next-xhr-backend';
+import LngDetector from 'i18next-browser-languagedetector';
+
+/**
+ * Set up the i18n library
+ * @returns {Promise} the result of setting up i18n
+ */
+export function setup() {
+    return new Promise((resolve, reject) => {
+        i18next
+            .use(LngDetector)
+            .use(XHR)
+            .init({
+                backend: {
+                    loadPath: '/static/locales/{{ns}}/{{lng}}.json'
+                },
+                detection: {
+                    order: [
+                        'querystring',
+                        'cookie',
+                        'localStorage',
+                        'navigator'
+                    ],
+                    lookupQuerystring: 'lng',
+                    lookupCookie: 'lng',
+                    lookupLocalStorage: 'lng',
+
+                    caches: ['localStorage', 'cookie']
+                },
+                debug: true,
+                ns: 'webmud',
+                defaultNS: 'webmud'
+            }, (err, t) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(t);
+                }
+            });
+    });
+}
--- webapp/src/main/javascript/main.js
+++ webapp/src/main/javascript/main.js
@@ -1,33 +1,12 @@
 import 'bootstrap';
 
 import {renderUi} from './ui';
-import i18next from 'i18next';
-import XHR from 'i18next-xhr-backend';
-import LngDetector from 'i18next-browser-languagedetector';
+import {setup} from './components/i18n/setup';
 
-i18next
-    .use(LngDetector)
-    .use(XHR)
-    .init({
-        backend: {
-            loadPath: '/static/locales/{{ns}}/{{lng}}.json'
-        },
-        detection: {
-            order: [
-                'querystring',
-                'cookie',
-                'localStorage',
-                'navigator'
-            ],
-            lookupQuerystring: 'lng',
-            lookupCookie: 'lng',
-            lookupLocalStorage: 'lng',
-
-            caches: ['localStorage', 'cookie']
-        },
-        debug: true,
-        ns: 'webmud',
-        defaultNS: 'webmud'
-}, (err, t) => {
-    renderUi();
-});
+setup()
+    .then(() => {
+        renderUi();
+    })
+    .catch((err) => {
+        console.log('Error configuring i18n', err);
+    });
