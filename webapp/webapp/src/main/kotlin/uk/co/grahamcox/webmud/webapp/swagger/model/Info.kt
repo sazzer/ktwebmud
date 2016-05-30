a1a28b9bee98dde78ec1602be13905d2256668b2
Graham Cox <graham@grahamcox.co.uk>
2016-05-30 16:20:45 +0100

    Added in the Swagger generator and controller

---
+++ webapp/webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/swagger/model/Info.kt
@@ -0,0 +1,9 @@
+package uk.co.grahamcox.ktwebmud.webapp.swagger.model
+
+/**
+ * The Information about the API as a whole
+ * @property title The title of the API
+ * @property version The version of the API
+ */
+data class Info(val title: String,
+                val version: String)
---
+++ webapp/webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/swagger/model/Response.kt
@@ -0,0 +1,24 @@
+package uk.co.grahamcox.ktwebmud.webapp.swagger.model
+
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+/**
+ * Representation of the response of an operation
+ * @property description The description of the response
+ * @param schemaLocation The location of the schema
+ */
+class Response(val description: String,
+               private val schemaLocation: String? = null) {
+
+    fun getSchema(): Map<String, Any>? {
+        return if (schemaLocation != null) {
+            val schemaPath = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .replacePath(schemaLocation).build().toUriString()
+            mapOf("\$ref" to schemaPath)
+        } else {
+            null
+        }
+    }
+
+}
