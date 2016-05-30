e51505ea3e0bf2af6ca69a35dc14c794dfd2a265
Graham Cox <graham@grahamcox.co.uk>
2016-05-30 18:48:13 +0100

    Downgraded Swagger-ui to one that's known to work with the schema definitions generated, and actually configure the social links controller to use it

--- webapp/webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/social/SocialController.kt
+++ webapp/webapp/src/main/kotlin/uk/co/grahamcox/webmud/webapp/social/SocialController.kt
@@ -1,9 +1,13 @@
 package uk.co.grahamcox.webmud.webapp.social
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import uk.co.grahamcox.ktwebmud.webapp.schemas.SocialLinkModel
+import uk.co.grahamcox.ktwebmud.webapp.swagger.annotations.SwaggerResponse
+import uk.co.grahamcox.ktwebmud.webapp.swagger.annotations.SwaggerSummary
+import uk.co.grahamcox.ktwebmud.webapp.swagger.annotations.SwaggerTags
 import java.net.URI
 
 /**
@@ -11,12 +15,15 @@ import java.net.URI
  */
 @RestController
 @RequestMapping("/api/social")
+@SwaggerTags(arrayOf("social"))
 class SocialController {
     /**
      * Get the details of the social sites that are connected
      * @return a collection of social site name and the URL that links to that site
      */
     @RequestMapping(method = arrayOf(RequestMethod.GET))
+    @SwaggerSummary("Get the social links that are configured")
+    @SwaggerResponse(statusCode = HttpStatus.OK, description = "The collection of configured social links", schema = "socialLinks.json")
     fun getSocialDetails() : List<SocialLinkModel> = listOf(
             SocialLinkModel().withId("facebook").withLink("http://www.facebook.com"),
             SocialLinkModel().withId("twitter").withLink("http://www.twitter.com"),
